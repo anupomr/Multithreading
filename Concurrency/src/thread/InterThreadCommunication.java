38f67e3a40c20fcee18769f5889bbd9c155a17de
anupo <anupomr@yahoo.com>
2020-06-13 08:55:45 -0400

    ProducerThread created

--- Concurrency/src/thread/InterThreadCommunication.java
+++ Concurrency/src/thread/InterThreadCommunication.java
@@ -1,5 +1,8 @@
 package thread;
 
+import java.util.Queue;
+import java.util.Random;
+
 public class InterThreadCommunication {
 
 	public static void main(String[] args) {
@@ -7,4 +10,35 @@ public class InterThreadCommunication {
 
 	}
 
+}
+class ProducerThread extends Thread {
+	Queue<Integer> q;
+
+	public ProducerThread(Queue<Integer> q, String name) {
+		super(name);
+		this.q = q;
+	}
+
+	public void run() {
+
+		while (true) {
+			synchronized (q) {
+				while (q.size() == 3) {
+					try {
+						System.out.println(Thread.currentThread().getName() +
+								" :q is full waiting consumer to consume");
+						q.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				Random r = new Random();
+				int i = r.nextInt();
+				q.add(i);
+				System.out.println(" Item Produce :  "+i);
+				q.notifyAll();
+			}
+		}
+	}
 }
