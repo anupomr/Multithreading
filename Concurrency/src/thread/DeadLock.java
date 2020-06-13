8bf88f35f3bb079af716e81ea74cdae00d88b8f0
anupo <anupomr@yahoo.com>
2020-06-13 11:00:03 -0400

    Dead Lock implemented

---
+++ Concurrency/src/thread/DeadLock.java
@@ -0,0 +1,25 @@
+package thread;
+
+public class DeadLock {
+	Resource resource1 = new Resource();
+	Resource resource2 = new Resource();
+
+	public void method1() {
+		synchronized (resource1) {
+			System.out.println("Aquired lock on resource1 by :" +
+					Thread.currentThread());
+			synchronized (resource2) {
+				System.out.println("Aquired lock on resource2 by :" +
+						Thread.currentThread());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		DeadLock d = new DeadLock();
+	}
+
+}
+
+class Resource {
+}
