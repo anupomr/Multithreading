cf2ed16063ad60a04d354444bb457f238e846ea8
anupo <anupomr@yahoo.com>
2020-06-15 17:16:16 -0400

    Resolve ConcurrentModificationException

--- Concurrency/src/thread/concurrent/ConHashMapImp.java
+++ Concurrency/src/thread/concurrent/ConHashMapImp.java
@@ -1,11 +1,13 @@
 package thread.concurrent;
 
-import java.util.HashMap;
+
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConHashMapImp extends Thread{
-	static HashMap<Integer, String> map = new HashMap<Integer, String>();
+	//static HashMap<Integer, String> map = new HashMap<Integer, String>();
+	static ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<Integer, String>();
 	public static void main(String[] args) {
 		ConHashMapImp tChild =new ConHashMapImp();
 		
@@ -13,13 +15,13 @@ public class ConHashMapImp extends Thread{
 		map.put(2, "Mo");
 		map.put(3, "Himadri");
 		map.put(4, "Apurba");
-		
+		//ConcurrentModificationException
 		tChild.start();
 		Iterator<Entry<Integer, String>> itr = map.entrySet().iterator();
 		while(itr.hasNext()) {
 			Entry entry=itr.next();
 			System.out.println("Key = " +entry.getKey()+
-					"Value = "+entry.getValue());
+					"  Value = "+entry.getValue());
 			
 			try {
 				Thread.sleep(100);
