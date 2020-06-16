3481a9d6f3a7f66de4533df887a675a3871d3d60
anupo <anupomr@yahoo.com>
2020-06-15 21:49:03 -0400

    CopyOnWriteArraySet Implemented

---
+++ Concurrency/src/thread/concurrent/CopyOnWriteArraySetImp.java
@@ -0,0 +1,22 @@
+package thread.concurrent;
+
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class CopyOnWriteArraySetImp {
+
+	public static void main(String[] args) {
+		CopyOnWriteArraySet<String> set = new CopyOnWriteArraySet<>();
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Anupom");
+		list.add("Anupom");
+		list.add("Stephen");
+		list.add("Stephen");
+		
+		// Make unique 
+		CopyOnWriteArraySet<String> set1 = new CopyOnWriteArraySet<>(list);
+		
+		System.out.println(set1);
+		}
+
+}
