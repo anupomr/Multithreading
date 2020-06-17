cfec4f936f892b88c370aff224f5b95fd60974e0
anupo <anupomr@yahoo.com>
2020-06-17 18:29:49 -0400

    Exception handled for CompletableFuture

--- Concurrency/src/asynchronous/ComFuture.java
+++ Concurrency/src/asynchronous/ComFuture.java
@@ -10,10 +10,15 @@ public class ComFuture {
 		ExecutorService es= Executors.newFixedThreadPool(4);
 		CompletableFuture.supplyAsync(()->1, es)
 		.thenApplyAsync(e -> addOne(e), es)
-		.thenApplyAsync(e -> addOne(e), es)
+		.thenApplyAsync(e -> addOne(e/0), es)
+		.exceptionally(throwable -> handleIt(throwable))
 		.thenAcceptAsync(e -> System.out.println(e), es)
 		.thenRunAsync(() -> es.shutdown());
 
+	}
+	private static int handleIt(Throwable t) {
+		System.out.println("Exception Occured "+t);
+		return 0;
 	}
 	private static int  addOne(int e) {
 		return e+1;
